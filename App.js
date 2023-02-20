ad3490e61bfa8a9d10cbeb4d799193628bf71d69
Morgan Nowak <mzeek88@gmail.com>
2023-02-20 15:16:57 -0500

    Basics set up

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import { Provider } from "react-redux";
+import { store } from "./store";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+
+import "./output.css";
+
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import MapScreen from "./screens/MapScreen";
+
+/*  
+1. Set up redux
+2. React-navigation
+3. nativeWind
+*/
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ components/NavOptions.js
@@ -0,0 +1,52 @@
+import { View, Text, FlatList, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import { Icon } from "@rneui/themed";
+import { useNavigation } from "@react-navigation/native";
+
+const data = [
+  {
+    id: "123",
+    title: "Get a ride",
+    image: "https://links.papareact.com/3pn",
+    screen: "MapScreen",
+  },
+  {
+    id: "456",
+    title: "Order food",
+    image: "https://links.papareact.com/28w",
+    screen: "EatScreen",
+  },
+];
+
+const NavOptions = () => {
+  const navigation = useNavigation();
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      horizontal
+      renderItem={({ item }) => (
+        <TouchableOpacity
+          onPress={() => navigation.navigate(item.screen)}
+          className="p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40"
+        >
+          <View>
+            <Image
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
+              source={{
+                uri: item.image,
+              }}
+            />
+            <Text className="mt-2 text-lg font-semibold">{item.title}</Text>
+
+            <View className="bg-black rounded-full w-10 h-10 items-center justify-center mt-4 p-2">
+              <Icon name="arrowright" color="white" type="antdesign" />
+            </View>
+          </View>
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
+
+export default NavOptions;
